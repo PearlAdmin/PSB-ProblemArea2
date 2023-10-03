3b284a9425fa9f2a8e237f313a5516b165129d17
Kyle Carlo Lasala <kyle_lasala@dlsu.edu.ph>
2023-10-04 00:37:55 +0800

    switch to page routing

--- pages/admin/index.tsx
+++
@@ -1,7 +0,0 @@
-export default function test() {
-    return (
-        <div>
-            <h1>Test 1</h1>
-        </div>
-    )
-}
---
+++ pages/login.tsx
@@ -0,0 +1,8 @@
+export default function Login() {
+    return (
+        <form action="">
+            <input type="text" placeholder="Username"/>
+            <input type="password" placeholder="Password"/>
+        </form>
+    )
+}
