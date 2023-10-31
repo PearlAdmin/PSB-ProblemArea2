24e3c4b963af3f94162809aa7d3a9ea25081e959
Kyle Carlo Lasala <kyle_lasala@dlsu.edu.ph>
2023-11-01 01:16:42 +0800

    strict false update

--- app/api/records/route.js
+++ app/api/records/route.js
@@ -2,17 +2,18 @@ import dbConnect from "@/libs/db";
 import {NextResponse} from "next/server";
 import mongoose from "mongoose";
 
-export default async (req, res) => {
+export async function POST(req) {
   try {
-    const { data } = await req.json(); // Assuming you're sending data in the request body
-
+    const data = await req.json(); // Assuming you're sending data in the request body
+    console.log(data);
     // Create a model for the data, but it's not required to have a schema
     const Record = mongoose.models.Record || mongoose.model('Record', {}, 'records');
 
     // Insert the data into the database
+    await dbConnect();
     await Record.create(data);
 
-    return NextResponse.json({message: "Question created successfully"}, {status: 201});
+    return NextResponse.json({message: "Record created successfully"}, {status: 201});
   } catch (error) {
     return NextResponse.json({message: error.message}, {status: 500});
   }
--- libs/db.js
+++ libs/db.js
@@ -2,7 +2,14 @@ import mongoose from 'mongoose';
 
 const dbConnect = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI);
+        await mongoose.connect(
+            process.env.MONGODB_URI,
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+                strict: false, // This disables strict mode, allowing dynamic schema
+            }
+        );
         console.log('Connected to database');
     } catch (error) {
         console.log(error);
