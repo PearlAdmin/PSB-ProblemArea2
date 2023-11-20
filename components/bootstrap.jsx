f664fec2b9dd88b18fadfce883749996f04fbfda
Kyle Carlo Lasala <kyle_lasala@dlsu.edu.ph>
2023-11-20 10:36:44 +0800

    RESET

--- components/bootstrap.jsx
+++
@@ -1,3 +0,0 @@
-"use client";
-
-export { Container, Alert, Spinner, Button, Pagination, Form, InputGroup } from "react-bootstrap";
--- models/records.js
+++
@@ -1,9 +0,0 @@
-import mongoose, { Schema } from "mongoose";
-
-const noStrictSchema = new Schema({
-  
-}, {strict: false});
-
-const Record = mongoose.models.Record || mongoose.model('Record', noStrictSchema, 'records');
-
-export default Record;
